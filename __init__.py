77e749619dbce194d1df63305945beef0b87ea91
ahmed boutayeb <ahmedboutayeb01@gmail.com>
2018-11-11 23:31:57 +0000

    Setting up Flask-Uploads for images

--- __init__.py
+++ __init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate
 from flaskext.markdown import Markdown
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+
 app = Flask(__name__)
 app.config.from_object('settings')
 db = SQLAlchemy(app)
@@ -10,7 +12,12 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # Markdown
-Markdown(app)
+md = Markdown(app, extensions=['fenced_code', 'tables'])
+
+#images
+uploaded_images = UploadSet('images', IMAGES)
+configure_uploads(app, uploaded_images)
+
 
 from blog import views
 from author import views
