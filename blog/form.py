953e5f988c3aebb80f16c7cec9d4ac2ee8e2320f
ahmed boutayeb <ahmedboutayeb01@gmail.com>
2018-11-12 12:11:19 +0000

    Adding images to blog posts and displaying the image in the article

--- blog/form.py
+++ blog/form.py
@@ -3,6 +3,7 @@ from wtforms import validators, StringField, TextAreaField
 from author.form import RegisterForm
 from blog.models import Category
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from flask_wtf.file import FileField, FileAllowed
 
 
 class SetupForm(RegisterForm):
@@ -15,6 +16,9 @@ def categories():
     return Category.query
 
 class PostForm(Form):
+    image = FileField('Image', validators=[
+        FileAllowed(['jpg', 'png'], 'Image only!')
+        ])
     title = StringField('Title', [
         validators.Required(),
         validators.Length(max=80)
--- settings.py
+++ settings.py
@@ -10,4 +10,4 @@ DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USERNAME, DB_PASSWORD,DB_HOST, BLOG
 SQLALCHEMY_DATABASE_URI = DB_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 UPLOADED_IMAGES_DEST = '/home/ubuntu/workspace/flask_blog/static/images'
-UPLOADED_IMAGES_URL = '/static/images'
+UPLOADED_IMAGES_URL = '/static/images/'
